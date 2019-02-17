f7beeb69eb5412a245cb51b982b5665734caa924
Agustin Cabra <cabra.agustin@gmail.com>
2019-02-17 02:02:45 +0100

    ClosestTime solved using java.util.LocalTime and testing all the possible options in the next 24 hrs

--- src/main/java/com/acabra/litcode/ClosestTime.java
+++ src/main/java/com/acabra/litcode/ClosestTime.java
@@ -1,7 +1,37 @@
 package com.acabra.litcode;
 
+import java.time.LocalTime;
+import java.time.format.DateTimeFormatter;
+import java.util.HashSet;
+import java.util.Set;
+
 public class ClosestTime {
+
     public String closestTime(String time) {
-        return "";
+        LocalTime localTime = LocalTime.parse(time, DateTimeFormatter.ofPattern("HH:mm"));
+        Set<Character> valid = getValidUnits(time);
+        while(true) {
+            localTime = localTime.plusMinutes(1);
+            if (valid(localTime.toString(), valid)) {
+                return localTime.toString();
+            }
+        }
+    }
+
+    private boolean valid(String time, Set<Character> valid) {
+        for (int i = 0; i < time.length(); i++) {
+            if (!valid.contains(time.charAt(i))) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    private Set<Character> getValidUnits(String time) {
+        Set<Character> valid = new HashSet<>();
+        for (int i = 0; i < time.length(); i++) {
+            valid.add(time.charAt(i));
+        }
+        return valid;
     }
 }
