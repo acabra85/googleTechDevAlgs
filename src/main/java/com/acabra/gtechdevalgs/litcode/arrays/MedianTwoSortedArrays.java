7754b41a5fff76acb2ccfed2eefcd8d5edc8be98
Agustin Cabra <cabra.agustin@gmail.com>
2019-03-05 04:40:57 +0100

    [refactor] removing unnecesary code and adding javadoc for Median Two
    Sorted arrays.

--- src/main/java/com/acabra/gtechdevalgs/litcode/arrays/MedianTwoSortedArrays.java
+++ src/main/java/com/acabra/gtechdevalgs/litcode/arrays/MedianTwoSortedArrays.java
@@ -4,31 +4,31 @@ import java.util.NoSuchElementException;
 import java.util.PriorityQueue;
 
 public class MedianTwoSortedArrays {
+
+    /**
+     * Find the median value of two sorted arrays using a min_heap to retrieve the next
+     * values until reaching half of them, if the total count is even return the sum of (last + next )/2
+     * or else return next
+     * @param arr1 the first sorted array
+     * @param arr2 second sorted array
+     * @return the median
+     */
     public double median(int[] arr1, int[] arr2) {
         if (null == arr1 || null == arr2) throw new NullPointerException("input must be non-null");
         if (arr1.length == 0) return medianSingle(arr2);
         if (arr2.length == 0) return medianSingle(arr1);
-        Data data1 = new Data(arr1);
-        Data data2 = new Data(arr2);
         PriorityQueue<Data> pq = new PriorityQueue<>();
-        if (data1.hasNext()) pq.offer(data1);
-        if (data2.hasNext()) pq.offer(data2);
-        int mid = (data1.size() + data2.size()) / 2;
-        if ((arr1.length + arr2.length )%2 == 0) {
-            int last = 0;
-            for (int i = 0; i < mid; i++) {
-                Data remove = pq.remove();
-                last = remove.next();
-                if (remove.hasNext()) pq.offer(remove);
-            }
-            return (last + pq.remove().next()) / 2.0d;
-        }
-        for (int j = 0; j < mid; j++) {
+        pq.offer(new Data(arr1));
+        pq.offer(new Data(arr2));
+        int totalCount = arr1.length + arr2.length;
+        int mid = totalCount / 2;
+        int last = 0;
+        for (int i = 0; i < mid; i++) {
             Data remove = pq.remove();
-            remove.next();
+            last = remove.next();
             if (remove.hasNext()) pq.offer(remove);
         }
-        return pq.remove().next();
+        return (totalCount %2 == 0) ? (last + pq.remove().next()) / 2.0d : pq.remove().next();
     }
 
     private double medianSingle(int[] arr1) {
@@ -41,7 +41,7 @@ public class MedianTwoSortedArrays {
 
     private class Data implements Comparable<Data> {
         final int[] arr;
-        int idx;
+        int idx = 0;
 
         private Data(int[] arr) {
             this.arr = arr;
