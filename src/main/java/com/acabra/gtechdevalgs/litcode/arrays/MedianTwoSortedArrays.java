25c5888887c104a412e14373685712c0d2c2e8c2
Agustin Cabra <cabra.agustin@gmail.com>
2019-03-05 23:08:25 +0100

    [improve] improving solution of Median2SorterArrays from O(n+m) to O(log(min(m,n)))

--- src/main/java/com/acabra/gtechdevalgs/litcode/arrays/MedianTwoSortedArrays.java
+++ src/main/java/com/acabra/gtechdevalgs/litcode/arrays/MedianTwoSortedArrays.java
@@ -6,6 +6,52 @@ import java.util.PriorityQueue;
 public class MedianTwoSortedArrays {
 
     /**
+     * Runs in O(log(min_size_array)), partition in two groups first group with all smaller elements
+     * and second group with overall larger elements, when partition completed the median is found
+     * in either in the largest element of first group for odd count of total elements or the half
+     * of the sum of the largest of first group and the smallest of second group for even count of
+     * total elements.
+     * @param arr1 the first sorted array of integers
+     * @param arr2 second sorted array of integers
+     * @return the median of the merged array on integers
+     */
+    public double median(int[] arr1, int[] arr2) {
+        if (null == arr1 || null == arr2) throw new NullPointerException("input must be non-null");
+        if (arr1.length == 0) return medianSingle(arr2);
+        if (arr2.length == 0) return medianSingle(arr1);
+
+        boolean isA1Larger = arr1.length > arr2.length;
+        int[] shorter1 = isA1Larger ? arr2 : arr1; //shorter1 contains the shortest of the two
+        int[] larger2 = isA1Larger ? arr1 : arr2; //larger2 contains the largest of the two
+
+        int low = 0;
+        int high = shorter1.length;
+
+        int part1, part2, maxLeft1, minRight1, maxLeft2, minRight2;
+        while (low <= high) {
+            part1 = (low + high) / 2;
+            part2 = (shorter1.length + larger2.length + 1) / 2 -part1;
+
+            maxLeft1 = part1 > 0 ? shorter1[part1 - 1] : Integer.MIN_VALUE;
+            minRight1 = part1 != shorter1.length ? shorter1[part1] : Integer.MAX_VALUE;
+
+            maxLeft2 = part2 > 0 ? larger2[part2 - 1] : Integer.MIN_VALUE;
+            minRight2= part2 != larger2.length ? larger2[part2] : Integer.MAX_VALUE;
+
+            if (maxLeft1 > minRight2) {
+                high = part1 - 1;
+            } else if (minRight1 < maxLeft2) {
+                low = part1 + 1;
+            } else { //partition completed
+                if ((shorter1.length + larger2.length) % 2 == 0) {
+                    return (Math.max(maxLeft1, maxLeft2) + Math.min(minRight1, minRight2)) / 2.0;
+                }
+                return Math.max(maxLeft1, maxLeft2);
+            }
+        }
+        return -1;
+    }
+    /**
      * Find the median value of two sorted arrays using a min_heap to retrieve the next
      * values until reaching half of them, if the total count is even return the sum of (last + next )/2
      * or else return next
@@ -13,7 +59,7 @@ public class MedianTwoSortedArrays {
      * @param arr2 second sorted array
      * @return the median
      */
-    public double median(int[] arr1, int[] arr2) {
+    public double median_n(int[] arr1, int[] arr2) {
         if (null == arr1 || null == arr2) throw new NullPointerException("input must be non-null");
         if (arr1.length == 0) return medianSingle(arr2);
         if (arr2.length == 0) return medianSingle(arr1);
--- src/test/java/com/acabra/gtechdevalgs/litcode/arrays/MedianTwoSortedArraysTest.java
+++ src/test/java/com/acabra/gtechdevalgs/litcode/arrays/MedianTwoSortedArraysTest.java
@@ -29,6 +29,33 @@ public class MedianTwoSortedArraysTest {
     public void should_return_1_test_2() {
         int[] arr1 = {1,1,1,1,1};
         int[] arr2 = {1,1};
+        Assert.assertEquals(1, new MedianTwoSortedArrays().median_n(arr1, arr2), 0.000001d);
+    }
+
+    @Test
+    public void should_return_2_test_1_O_n() {
+        int[] arr1 = {1,3};
+        int[] arr2 = {2};
+        Assert.assertEquals(2, new MedianTwoSortedArrays().median_n(arr1, arr2), 0.000001d);
+    }
+    @Test
+    public void should_return_2f5_test_2_O_n() {
+        int[] arr1 = {1,2};
+        int[] arr2 = {3,4};
+        Assert.assertEquals(2.5, new MedianTwoSortedArrays().median_n(arr1, arr2), 0.000001d);
+    }
+
+    @Test
+    public void should_return_0f5_test_3_O_n() {
+        int[] arr1 = {1,1,1,1,1};
+        int[] arr2 = {1};
+        Assert.assertEquals(1, new MedianTwoSortedArrays().median_n(arr1, arr2), 0.000001d);
+    }
+
+    @Test
+    public void should_return_1_test_2_O_n() {
+        int[] arr1 = {1,1,1,1,1};
+        int[] arr2 = {1,1};
         Assert.assertEquals(1, new MedianTwoSortedArrays().median(arr1, arr2), 0.000001d);
     }
 }
