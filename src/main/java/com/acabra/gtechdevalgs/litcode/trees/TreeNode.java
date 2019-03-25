115438449ed6dfff84554436b3fb961da1b8bc4e
Agustin Cabra Sarmiento <sarmiea@amazon.com>
2019-03-25 21:56:04 +0100

    [refactor] including toString method for treeNode for debugging purposes

--- src/main/java/com/acabra/gtechdevalgs/litcode/trees/TreeNode.java
+++ src/main/java/com/acabra/gtechdevalgs/litcode/trees/TreeNode.java
@@ -12,4 +12,9 @@ public class TreeNode {
     public TreeNode(int val) {
         this.val = val;
     }
+
+    @Override
+    public String toString() {
+        return (left!=null ? left.val : ".")  + "@"+ val+"@"+(right!=null?right.val:".") ;
+    }
 }
