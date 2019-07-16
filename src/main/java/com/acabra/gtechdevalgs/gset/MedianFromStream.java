da1f1def90d50bcb0b7d37456494d77170102c10
Agustin Cabra <cabra.agustin@gmail.com>
2019-07-16 02:02:42 +0200

    [refactor] adding metrics to the MedianFromStream class to compare to bruteforce algorithm that sorts every-time.

--- src/main/java/com/acabra/gtechdevalgs/gset/MedianFromStream.java
+++ src/main/java/com/acabra/gtechdevalgs/gset/MedianFromStream.java
@@ -3,10 +3,11 @@ package com.acabra.gtechdevalgs.gset;
 import java.util.Comparator;
 import java.util.NoSuchElementException;
 import java.util.PriorityQueue;
+import java.util.function.Consumer;
 
-public class MedianFromStream {
+public class MedianFromStream implements Consumer<Double> {
 
-    private static final Comparator<Double> COMP = new Comparator<Double>() {
+    private static final Comparator<Double> DESCENDING_ORDER_COMPARATOR = new Comparator<Double>() {
         @Override
         public int compare(Double o1, Double o2) {
             return o1 < o2 ? 1 : o1 == o2 ? 0 : -1;
@@ -14,34 +15,42 @@ public class MedianFromStream {
     };
 
     // lower elements will be a MaxHeap, since the biggest element here will be smaller than the highest element on the other queue
-    private PriorityQueue<Double> lowerElements = new PriorityQueue<>(COMP);
+    private PriorityQueue<Double> lowerElements = new PriorityQueue<>(DESCENDING_ORDER_COMPARATOR);
 
     private PriorityQueue<Double> higherElements = new PriorityQueue<>();
 
     public double getMedian() {
         if (this.lowerElements.isEmpty()) throw new NoSuchElementException("no elements present");
-        PriorityQueue<Double> smaller = this.lowerElements.size() < this.higherElements.size() ? this.lowerElements : this.higherElements;
-        PriorityQueue<Double> bigger = smaller == this.lowerElements ? this.higherElements : this.lowerElements;
+        PriorityQueue<Double> smaller = getSmaller();
+        PriorityQueue<Double> bigger = getBigger(smaller);
         if (smaller.size() == bigger.size()) {
             return (lowerElements.peek() + higherElements.peek()) / 2.0;
         }
         return bigger.peek();
     }
 
-    public void add(double num) {
+    private PriorityQueue<Double> getBigger(PriorityQueue<Double> smaller) {
+        return smaller == lowerElements ? higherElements : lowerElements;
+    }
+
+    private PriorityQueue<Double> getSmaller() {
+        return lowerElements.size() < higherElements.size() ? lowerElements : higherElements;
+    }
+
+    @Override
+    public void accept(Double num) {
         if (lowerElements.isEmpty() || num < lowerElements.peek()) {
             lowerElements.offer(num);
         } else {
             higherElements.offer(num);
         }
-        rebalance();
+        reBalance();
     }
 
-    private void rebalance() {
-        PriorityQueue<Double> smaller = this.lowerElements.size() < this.higherElements.size() ? this.lowerElements : this.higherElements;
-        PriorityQueue<Double> bigger = smaller == this.lowerElements ? this.higherElements : this.lowerElements;
-        if (bigger.size() - smaller.size() > 1) {
-            smaller.offer(bigger.poll());
+    private void reBalance() {
+        if (Math.abs(lowerElements.size() - higherElements.size()) > 1) {
+            PriorityQueue<Double> smaller = getSmaller();
+            smaller.offer(getBigger(smaller).poll());
         }
     }
 }
--- src/test/java/com/acabra/gtechdevalgs/gset/MedianFromStreamTest.java
+++ src/test/java/com/acabra/gtechdevalgs/gset/MedianFromStreamTest.java
@@ -1,17 +1,65 @@
 package com.acabra.gtechdevalgs.gset;
 
+import com.google.common.base.Stopwatch;
 import org.junit.Assert;
 import org.junit.Test;
 
+import java.security.SecureRandom;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.atomic.AtomicLong;
+import java.util.function.LongBinaryOperator;
+
 public class MedianFromStreamTest {
 
+    private AtomicLong totalBF = new AtomicLong();
+    private AtomicLong totalFast = new AtomicLong();
+
+    /**
+     * For 10 items Brute force is ~1600 times slower than Fast implementation
+     * For 100 items Brute force is ~400 times slower than Fast implementation
+     * For 1000 items Brute force is ~50 times slower than Fast implementation
+     * For 10000 items Brute force is ~100 times slower than Fast implementation
+     * For 100000 items Brute force is ~1300 times slower than Fast implementation
+     */
     @Test
     public void test_api() {
         MedianFromStream median = new MedianFromStream();
-        median.add(1.0);
-        median.add(2.0);
-        Assert.assertEquals(1.5, median.getMedian(), 0.001);
-        median.add(3.0);
-        Assert.assertEquals(2.0, median.getMedian(), 0.001);
+        SecureRandom sr = new SecureRandom();
+        int CAP = 100000;
+        List<Double> nums = new ArrayList<>(CAP);
+        double expected;
+        LongBinaryOperator OP = new LongBinaryOperator() {
+            @Override
+            public long applyAsLong(long left, long right) {
+                return left + right;
+            }
+        };
+        double actual;
+        for (int i = 0; i < CAP; i++) {
+            double num = sr.nextDouble();
+            median.accept(num);
+            nums.add(num);
+            Stopwatch bf = Stopwatch.createStarted();
+            expected = calculateMedian(nums);
+            totalBF.accumulateAndGet(bf.stop().elapsed(TimeUnit.NANOSECONDS), OP);
+            Stopwatch fast = Stopwatch.createStarted();
+            actual = median.getMedian();
+            totalFast.accumulateAndGet(fast.stop().elapsed(TimeUnit.NANOSECONDS), OP);
+            Assert.assertEquals(expected, actual, 0.01);
+        }
+        System.out.println("  bf: " + totalBF);
+        System.out.println("fast: " + totalFast);
+        System.out.println("factor of: " + (totalBF.get() * 1.0 / (totalFast.get() * 1.0)));
+    }
+
+    private static double calculateMedian(List<Double> nums) {
+        nums.sort(Double::compareTo);
+        int mid = nums.size() / 2;
+        if(nums.size() % 2 == 0) {
+            return (nums.get(mid - 1) + nums.get(mid)) / 2.0;
+        }
+        return nums.get(mid);
     }
 }
