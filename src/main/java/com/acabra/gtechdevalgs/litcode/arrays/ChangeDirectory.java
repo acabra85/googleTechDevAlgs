ffa3ef2cd4eac26171596bd404c794eece3e4e92
Agustin Cabra <sarmiea@amazon.com>
2020-03-17 15:23:47 +0100

    [solved] Java implementation of chdir UNIX (cd) change directory.

--- src/main/java/com/acabra/gtechdevalgs/litcode/arrays/ChangeDirectory.java
+++ src/main/java/com/acabra/gtechdevalgs/litcode/arrays/ChangeDirectory.java
@@ -1,18 +1,55 @@
 package com.acabra.gtechdevalgs.litcode.arrays;
 
+import java.util.ArrayDeque;
+import java.util.StringTokenizer;
+
 /**
- * Java implementation of UNIX chdir (cd) command.
+ * Java limited implementation of UNIX chdir (cd) command.
  */
 public class ChangeDirectory {
 
-    private final String PARENT_TOKEN = "..";
-    private final String PATH_DELIMITER = "/";
+    private static final String PARENT_TOKEN = "..";
+    private static final String PATH_DELIMITER = "/";
 
 
     public static String changeDir(String input) {
         if (null == input || input.length() == 0) {
             return "";
         }
-        return null;
+        boolean reachedRoot = false;
+        ArrayDeque<String> deque = new ArrayDeque<>();
+        StringTokenizer st = new StringTokenizer(input, PATH_DELIMITER, false);
+        while (st.hasMoreTokens()) {
+            String next = st.nextToken();
+            if (next.equals(PARENT_TOKEN)) {
+                if (!deque.isEmpty()) {
+                    deque.pollFirst();
+                } else {
+                    reachedRoot = true;
+                }
+            } else {
+                deque.offerFirst(next);
+            }
+        }
+        if (deque.isEmpty())
+            return "/";
+        return buildPathFromStack(deque, isAbsolute(input) || reachedRoot);
+    }
+
+    private static boolean isAbsolute(String input) {
+        return input.charAt(0) == '/';
+    }
+
+    private static String buildPathFromStack(ArrayDeque<String> deque, boolean includeRoot) {
+        StringBuilder sb = new StringBuilder();
+        if (includeRoot) {
+            sb.append("/");
+        }
+        while (!deque.isEmpty()) {
+            sb.append(deque.removeLast());
+            if (!deque.isEmpty())
+                sb.append("/");
+        }
+        return sb.toString();
     }
 }
--- src/test/java/com/acabra/gtechdevalgs/litcode/arrays/ChangeDirectoryTest.java
+++ src/test/java/com/acabra/gtechdevalgs/litcode/arrays/ChangeDirectoryTest.java
@@ -21,4 +21,66 @@ public class ChangeDirectoryTest {
         String expected = "";
         Assert.assertEquals(expected, ChangeDirectory.changeDir(input));
     }
+
+    @Test
+    public void should_return_self() {
+        String input = "somedirname";
+        Assert.assertEquals(input, ChangeDirectory.changeDir(input));
+    }
+
+    @Test
+    public void should_return_v() {
+        String input = "/a/../../../../v";
+        String expected = "/v";
+        Assert.assertEquals(expected, ChangeDirectory.changeDir(input));
+    }
+
+    @Test
+    public void should_return_root_1() {
+        String input = "/a/../../../..";
+        String expected = "/";
+        Assert.assertEquals(expected, ChangeDirectory.changeDir(input));
+    }
+
+    @Test
+    public void should_return_root_2() {
+        String input = "..";
+        String expected = "/";
+        Assert.assertEquals(expected, ChangeDirectory.changeDir(input));
+    }
+
+    @Test
+    public void should_return_root_3() {
+        String input = "/../../../..";
+        String expected = "/";
+        Assert.assertEquals(expected, ChangeDirectory.changeDir(input));
+    }
+
+    @Test
+    public void should_return_root_4() {
+        String input = "../../../..";
+        String expected = "/";
+        Assert.assertEquals(expected, ChangeDirectory.changeDir(input));
+    }
+
+    @Test
+    public void should_return_root_c_d() {
+        String input = "../../../c/d/e/..";
+        String expected = "/c/d";
+        Assert.assertEquals(expected, ChangeDirectory.changeDir(input));
+    }
+
+    @Test
+    public void should_return_root_c() {
+        String input = "/../../../c/d/../e/..";
+        String expected = "/c";
+        Assert.assertEquals(expected, ChangeDirectory.changeDir(input));
+    }
+
+    @Test
+    public void should_return_root_c_e() {
+        String input = "/../../../c/d/../e";
+        String expected = "/c/e";
+        Assert.assertEquals(expected, ChangeDirectory.changeDir(input));
+    }
 }
