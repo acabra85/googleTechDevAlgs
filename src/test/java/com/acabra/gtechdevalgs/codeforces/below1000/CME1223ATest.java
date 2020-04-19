8d848afa9ba5933bac7c18887f6150176ec3c42d
Agustin Cabra <cabra.agustin@gmail.com>
2020-04-19 18:42:15 +0200

    [codeforces] solution 1223A

---
+++ src/test/java/com/acabra/gtechdevalgs/codeforces/below1000/CME1223ATest.java
@@ -0,0 +1,13 @@
+package com.acabra.gtechdevalgs.codeforces.below1000;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class CME1223ATest {
+
+    @Test
+    public void should_return_0() {
+        Assert.assertEquals(1, CME1223A.Solution.solution(4));
+    }
+
+}
