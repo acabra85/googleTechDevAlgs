7c20f47b5fec990553bd9d7712253f78248677a2
Agustin Cabra <cabra.agustin@gmail.com>
2021-04-07 23:03:30 +0200

    [new solution] backtracking to collect the max gold

--- src/main/java/com/acabra/gtechdevalgs/google/PathWithMaximumGold2D.java
+++ src/main/java/com/acabra/gtechdevalgs/google/PathWithMaximumGold2D.java
@@ -11,9 +11,47 @@ public class PathWithMaximumGold2D {
      * 3. YOu are only allowed to cells containing gold.
      * 4. Once you enter a cell with gold all must be taken
      * 5. Constraints at most 25 cells contain gold
+     *
      * @return max gold possible to take
      */
     public int findPathWithMaxGold(int[][] mine) {
-        return -1;
+        if (mine == null || mine.length == 0) return 0;
+        this.R = mine.length;
+        this.C = mine[0].length;
+        this.mine = mine;
+        this.maxGold = 0;
+        for (int i = 0; i < R; ++i) {
+            for (int j = 0; j < C; ++j) {
+                if(mine[i][j] != 0)
+                    dfs(i, j, 0);
+            }
+        }
+        return maxGold;
     }
+
+    private void dfs(int i, int j, int collected) {
+        int cellValue = mine[i][j];
+        maxGold = Math.max(collected + cellValue, maxGold);
+
+        mine[i][j] = 0; //take gold
+
+        for (int[] availableMove : AVAILABLE_MOVES) {
+            int nx = i + availableMove[0];
+            int ny = j + availableMove[1];
+            if(validMove(nx, ny)) {
+                dfs(nx, ny, collected + cellValue);
+            }
+        }
+        mine[i][j] = cellValue;
+    }
+
+    private boolean validMove(int nx, int ny) {
+        return nx >= 0 && nx < R && ny>=0 && ny < C && mine[nx][ny] != 0;
+    }
+
+    static final int[][] AVAILABLE_MOVES = {{0, 1}, {1, 0}, {-1, 0}, {0, -1}};
+    int R;
+    int maxGold;
+    int C;
+    int[][] mine;
 }
--- src/test/java/com/acabra/gtechdevalgs/google/PathWithMaximumGold2DTest.java
+++ src/test/java/com/acabra/gtechdevalgs/google/PathWithMaximumGold2DTest.java
@@ -1,10 +1,8 @@
 package com.acabra.gtechdevalgs.google;
 
-import org.junit.Test;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
-
-import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Test;
 
 class PathWithMaximumGold2DTest {
 
@@ -25,6 +23,19 @@ class PathWithMaximumGold2DTest {
                 {1, 9, 0, 20},
         };
         int expected = 24;
+        Assertions.assertEquals(expected, underTest.findPathWithMaxGold(mine));
+    }
+
+    @Test
+    public void mustReturn20() {
+        int[][] mine = {
+                {1, 0, 0, 1},
+                {2, 0, 0, 2},
+                {3, 4, 2, 1},
+                {0, 0, 0, 0},
+                {1, 9, 0, 20},
+        };
+        int expected = 20;
         Assertions.assertEquals(expected, underTest.findPathWithMaxGold(mine));
     }
 }
