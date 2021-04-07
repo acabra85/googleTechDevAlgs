c8d69586552afdf5e16319d5a7cc52602c4a2d20
Agustin Cabra <cabra.agustin@gmail.com>
2021-04-07 23:14:47 +0200

    [new problem] minimal cost of typing a word in a english 2d-keyboard

---
+++ src/main/java/com/acabra/gtechdevalgs/google/MinimumCostTypingWord.java
@@ -0,0 +1,17 @@
+package com.acabra.gtechdevalgs.google;
+
+public class MinimumCostTypingWord {
+
+    /**
+     * Given an english keyboard (represented as a 2D array of 5-rows * 6-columns where the top left corner
+     * contains the letter A and the Z is located on the last row and the second column) and a word,
+     * find the minimal cost (total distance traveled by fingers within the keyboard)
+     * to type it. At most 1 finger on each hand can be used.
+     * The distance cost between 2 letters within the keyboard at coordinates (x1,y1) and (x2,y2)
+     * is given by the formula Abs(x1 - x2) + Abs(y1 - y2).
+     * @param word the minimal cost of typing a word in a keyboard
+     */
+    public int minimalCostToType(String word) {
+        return -1;
+    }
+}
---
+++ src/test/java/com/acabra/gtechdevalgs/google/MinimumCostTypingWordTest.java
@@ -0,0 +1,43 @@
+package com.acabra.gtechdevalgs.google;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class MinimumCostTypingWordTest {
+
+    private MinimumCostTypingWord underTest;
+
+    @BeforeEach
+    void setUp() {
+        this.underTest = new MinimumCostTypingWord();
+    }
+
+    @Test
+    void mustReturn3() {
+        String word = "CAKE";
+        int expected = 3;
+        Assertions.assertEquals(expected, underTest.minimalCostToType(word));
+    }
+
+    @Test
+    void mustReturn6() {
+        String word = "HAPPY";
+        int expected = 6;
+        Assertions.assertEquals(expected, underTest.minimalCostToType(word));
+    }
+
+    @Test
+    void mustReturn0_a() {
+        String word = "AAAZZZ";
+        int expected = 0;
+        Assertions.assertEquals(expected, underTest.minimalCostToType(word));
+    }
+
+    @Test
+    void mustReturn0_b() {
+        String word = "AZAZAZ";
+        int expected = 0;
+        Assertions.assertEquals(expected, underTest.minimalCostToType(word));
+    }
+}
