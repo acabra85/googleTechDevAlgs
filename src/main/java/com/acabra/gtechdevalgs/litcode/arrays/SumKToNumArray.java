410663a072e81e225056e73dc48cc6de4abbff07
Agustin Cabra <cabra.agustin@gmail.com>
2021-04-11 21:40:22 +0200

    [new solution] summing 2 numbers and returning their sum as an array representation

--- src/main/java/com/acabra/gtechdevalgs/litcode/arrays/SumKToNumArray.java
+++ src/main/java/com/acabra/gtechdevalgs/litcode/arrays/SumKToNumArray.java
@@ -1,7 +1,9 @@
 package com.acabra.gtechdevalgs.litcode.arrays;
 
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
+import java.util.Stack;
 
 public class SumKToNumArray {
 
@@ -14,6 +16,43 @@ public class SumKToNumArray {
      * @return the array representation of their sum
      */
     public List<Integer> sumKToArray(int[] num, int k) {
-        return Collections.emptyList();
+        char[] strK = String.valueOf(k).toCharArray();
+        Stack<Integer> q = new Stack<>();
+        int lenK = strK.length;
+        int lenNum = num.length;
+        int div = 0, i, j, val;
+        for(i = lenK-1, j=lenNum-1; i>=0 && j>=0;--i,--j) {
+            val = num[j] + (strK[i] - '0') + div;
+            div = val / 10;
+            q.push(val % 10);
+        }
+        if(lenK > lenNum) {
+            for(;i >=0;--i) {
+                if (div > 0) {
+                    val = div + (strK[i] - '0');
+                    div = val / 10;
+                    q.push(val % 10);
+                } else {
+                    q.push(strK[i] - '0');
+                }
+            }
+        } else {
+            for(; j>=0;--j) {
+                if(div > 0) {
+                    val = div + num[j];
+                    div = val / 10;
+                    q.push(val % 10);
+                } else {
+                    q.push(num[j]);
+                }
+            }
+        }
+        if(div > 0) {
+            q.push(div);
+        }
+        return new ArrayList<Integer>(){{
+            while(!q.isEmpty())
+                add(q.pop());
+        }};
     }
 }
