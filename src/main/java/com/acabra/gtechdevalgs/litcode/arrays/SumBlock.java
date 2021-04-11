6acfaed79b7727f247684c6f9238b212f5292236
Agustin Cabra <cabra.agustin@gmail.com>
2021-04-11 21:54:15 +0200

    [new solution] SumBlock solution

--- src/main/java/com/acabra/gtechdevalgs/litcode/arrays/SumBlock.java
+++ src/main/java/com/acabra/gtechdevalgs/litcode/arrays/SumBlock.java
@@ -10,6 +10,27 @@ public class SumBlock {
      * @return a new matrix same size as input where each cell corresponds to the sum described
      */
     public int[][] matrixBlockSum(int[][] matrix, int k) {
-        return null;
+        int R = matrix.length, C=matrix[0].length;
+        int[][] res = new int[R][C];
+        for(int i =0;i<R;++i) {
+            for(int j=0;j<C;++j) {
+                res[i][j] = sumOfAllRCs(matrix, i, j, k, R, C);
+            }
+        }
+        return res;
+    }
+
+    int sumOfAllRCs(int[][] mat, int i, int j, int k, int R, int C) {
+        int val = 0;
+        int ipk = Math.min(R-1, i + k);
+        int jpk = Math.min(C-1, j + k);
+        int lpk = Math.max(0,i - k);
+        int hpk =  Math.max(j - k, 0);
+        for(int r = lpk; r <= ipk ; ++r) {
+            for(int c = hpk; c <= jpk; ++c) {
+                val += mat[r][c];
+            }
+        }
+        return val;
     }
 }
