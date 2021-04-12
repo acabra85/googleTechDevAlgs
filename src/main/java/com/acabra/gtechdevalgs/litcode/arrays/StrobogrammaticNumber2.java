f76ae9ba81741b77b859b1f80a717637c7a2957e
Agustin Cabra <sarmiea@amazon.com>
2021-04-12 11:07:26 +0200

    [refactor] solving StrobogrammaticNumber2 without recursion

--- src/main/java/com/acabra/gtechdevalgs/litcode/arrays/StrobogrammaticNumber2.java
+++ src/main/java/com/acabra/gtechdevalgs/litcode/arrays/StrobogrammaticNumber2.java
@@ -7,33 +7,27 @@ import java.util.List;
  * Describe your class
  */
 public class StrobogrammaticNumber2 {
-    static final List<String> BASE_0 = new ArrayList<String>(){{add("");}};
-    static final List<String> BASE_1 = new ArrayList<String>(){{
-        add("0");
-        add("1");
-        add("8");
-    }};
     public List<String> findStrobogrammatic(int n) {
-        return solve(n, n);
-    }
-
-    private List<String> solve(int n, int target) {
-        if (n == 0) {
-            return BASE_0;
-        } else if (n == 1) {
-            return BASE_1;
-        }
-        List<String> prev = solve(n-2, target); //adding 2 elements on each side
-        List<String> res = new ArrayList<>((n<target?5:4) * prev.size());
-        for(String val: prev) {
-            if(n < target) {
-                res.add("0" + val + "0" );
+        List<String> BASE_0 = new ArrayList<>(){{add("");}};
+        List<String> BASE_1 = new ArrayList<>(){{
+            add("0");
+            add("1");
+            add("8");
+        }};
+        for (int i = 2; i <= n; ++i) {
+            List<String> res = new ArrayList<>((i < (n - 1)?5:4) * BASE_0.size());
+            for(String val: BASE_0) {
+                if(i < n - 1) {
+                    res.add("0" + val + "0" );
+                }
+                res.add("1" + val + "1");
+                res.add("6" + val + "9");
+                res.add("8" + val + "8");
+                res.add("9" + val + "6");
             }
-            res.add("1" + val + "1");
-            res.add("6" + val + "9");
-            res.add("8" + val + "8");
-            res.add("9" + val + "6");
+            BASE_0 = BASE_1;
+            BASE_1 = res;
         }
-        return res;
+        return BASE_1;
     }
 }
