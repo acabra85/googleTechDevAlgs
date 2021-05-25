1e6c737fc36d43ba0c831201ddd3480a82a13a00
Agustin Cabra <sarmiea@amazon.com>
2021-05-25 10:34:01 +0200

    [solution] fully justify a text given a maximum width per line

--- src/main/java/com/acabra/gtechdevalgs/litcode/arrays/JustifyText.java
+++ src/main/java/com/acabra/gtechdevalgs/litcode/arrays/JustifyText.java
@@ -1,5 +1,7 @@
 package com.acabra.gtechdevalgs.litcode.arrays;
 
+import java.util.ArrayDeque;
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
@@ -19,6 +21,93 @@ public class JustifyText {
      * @return
      */
     public List<String> justifyText(String[] words, int maxWidth) {
-        return Collections.emptyList();
+        if (words == null || words.length == 0) return Collections.emptyList();
+        if (words.length == 1) new Line(maxWidth, words[0], true).lastString();
+        List<String> res = new ArrayList<>();
+        Line line = new Line(maxWidth, words[0], false);///check TODO
+        for(int i=1;i<words.length;++i) {
+            String word = words[i];
+            if(line.canAppend(word)) {
+                line.appendWord(word);
+            } else {
+                res.add(line.toString());
+                line = new Line(maxWidth, word, false); //TODO CHECK
+            }
+        }
+        res.add(line.lastString());
+        return res;
+    }
+
+    static class Line {
+        final ArrayDeque<String> aq;
+        int length;
+        int spaces = 0;
+        final int maxLength;
+        final private boolean isLastLine;
+
+        Line(int mLength, String word, boolean isLastLine){
+            aq = new ArrayDeque<>();
+            aq.addLast(word);
+            this.length = word.length();
+            this.maxLength = mLength;
+            this.isLastLine = isLastLine;
+        }
+
+        boolean canAppend(String word) {
+            return this.length + word.length() + spaces <= maxLength;
+        }
+
+        void appendWord(String word) {
+            if (canAppend(word)) {
+                this.length += word.length();
+                ++spaces;
+                aq.addLast(word);
+                return;
+            }
+            throw new RuntimeException("invalid input");
+        }
+
+        public String lastString() {
+            StringBuilder sb = new StringBuilder();
+            while(!aq.isEmpty()) {
+                sb.append(aq.removeFirst());
+                if(spaces > 0) {
+                    sb.append(" ");
+                    --spaces;
+                }
+            }
+            sb.append(" ".repeat(this.maxLength - sb.length()));
+            return sb.toString();
+        }
+
+        @Override
+        public String toString() {
+            if(this.isLastLine) {
+                return this.lastString();
+            }
+            int missingSpace = this.maxLength  - this.length;
+            StringBuilder sb = new StringBuilder();
+            if(missingSpace == 0) {
+                while(!aq.isEmpty()) {
+                    sb.append(aq.removeFirst());
+                    if(spaces > 0) {
+                        sb.append(" ");
+                        --spaces;
+                    }
+                }
+                return sb.toString();
+            }
+            int div = missingSpace / spaces;
+            int mod = missingSpace % spaces;
+            while(!aq.isEmpty()) {
+                sb.append(aq.removeFirst());
+                if(spaces > 0) {
+                    sb.append(" ".repeat(1 + div + (mod>0?1:0)));
+                    --mod;
+                    --spaces;
+                }
+            }
+            return sb.toString();
+        }
     }
 }
