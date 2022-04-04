de9b859c022bd134cfb938bd94dbbe239cf679e2
Agustin Cabra <cabra.agustin@gmail.com>
2022-04-04 22:16:18 +0200

    [wip] attempting problem P4-codejam2022 QRound

---
+++ src/test/java/com/acabra/gtechdevalgs/gset/cjam2022/qround/P4_MaxFunWindsTest.java
@@ -0,0 +1,10 @@
+package com.acabra.gtechdevalgs.gset.cjam2022.qround;
+
+import org.junit.jupiter.api.Test;
+
+public class P4_MaxFunWindsTest {
+    @Test
+    public void test() throws Exception {
+        P4_MaxFunWinds.Solution.ofTestFile("cjam2022/qround/p4.txt").read();
+    }
+}
