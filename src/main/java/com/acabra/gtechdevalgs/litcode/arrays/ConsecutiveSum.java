16ae1b23870faf7507dcffbea512a9d2d8af17b4
Agustin Cabra <cabra.agustin@gmail.com>
2024-12-23 11:42:23 +0100

    bugfix - Add support for zero sums
    * upgrade jdk to 21
    * support zero targets
    * support index base response (range start, end)

--- src/main/java/com/acabra/gtechdevalgs/litcode/arrays/ConsecutiveSum.java
+++ src/main/java/com/acabra/gtechdevalgs/litcode/arrays/ConsecutiveSum.java
@@ -4,8 +4,8 @@ import java.util.ArrayDeque;
 
 public class ConsecutiveSum {
 
-    // input: unsorted array of nonnegative integers, integer representing sum.
-// output: continuous subarray with elements adding to the given sum.
+    // input: unsorted array of non-negative integers, integer representing sum.
+    // output: continuous subarray with elements adding to the given sum.
 
     // example:
     // 	input: arr = (1, 5, 3, 0, 7, 2, 4), sum = 12
@@ -13,33 +13,92 @@ public class ConsecutiveSum {
     public int[] consecutiveSum(int[] arr, int sum) {
         if(null == arr || arr.length == 0) return new int[]{};
         if(arr.length == 1 && arr[0] == sum) return arr;
-
-        int start = 0;
-        int end = 0;
-        int actualSum  = 0;
-        ArrayDeque<Integer> dq = new ArrayDeque<>();
-        while (end < arr.length && sum != actualSum) {
-            if(actualSum < sum) {
-                actualSum += arr[end];
-                dq.addLast(arr[end]);
-                ++end;
-            } else if(actualSum > sum) {
-                actualSum -= arr[start];
-                ++start;
-                dq.removeFirst();
-            }
-            if(actualSum == sum) {
-                return toIntArray(dq);
-            }
+        SumValue actualSum = getSumValue(arr, sum);
+        if (actualSum.isTarget()) {
+            return actualSum.resultAsSubsequence();
         }
         return new int[]{};
     }
 
-    private int[] toIntArray(ArrayDeque<Integer> dq) {
+    // returns the first occurrence in the array of the consecutive terms that add
+    // to the target sum [start, end]
+    // example:
+    // 	input: arr = (1, 5, 3, 0, 7, 2, 4), sum = 12
+    // 	output: [2, 5]
+    public int[] consecutiveSumIndex(int[] arr, int sum) {
+        if(null == arr || arr.length == 0) return new int[]{};
+        if(arr.length == 1 && arr[0] == sum) return arr;
+        SumValue actualSum = getSumValue(arr, sum);
+        if (actualSum.isTarget()) {
+            return actualSum.result();
+        }
+        return new int[]{};
+    }
+
+    private int[] toIntArray(ArrayDeque<Element> dq) {
         int[] ints = new int[dq.size()];
         for (int i = 0; i < ints.length; ++i) {
-            ints[i] = dq.removeFirst();
+            ints[i] = dq.removeFirst().value;
         }
         return ints;
+    }
+
+    private SumValue getSumValue(int[] arr, int sum) {
+        SumValue actualSum = new SumValue(sum);
+        for (int i = 0; i < arr.length; ++i) {
+            if (actualSum.bigger()) {
+                actualSum.fix();
+            }
+            if (actualSum.isTarget()) {
+                return actualSum;
+            }
+            actualSum.add(arr[i], i);
+        }
+        if (actualSum.bigger()) {
+            actualSum.fix();
+        }
+        return actualSum;
+    }
+
+    private record Element(int value, int idx) {}
+
+    private class SumValue {
+        Integer val = null;
+        ArrayDeque<Element> elements = new ArrayDeque<>();
+        final int target;
+
+        public SumValue(int target) {
+            this.target = target;
+        }
+
+        public boolean isTarget() {
+            return this.val != null && val == this.target;
+        }
+
+        public boolean bigger() {
+            return val != null && val > this.target;
+        }
+
+        public void add(int toAdd, int idx) {
+            elements.add(new Element(toAdd, idx));
+            val = val == null ? toAdd : val + toAdd;
+        }
+
+        public int[] result() {
+            return new int[]{elements.getFirst().idx, elements.getLast().idx};
+        }
+
+        public void fix() {
+            while (val > target && !elements.isEmpty()) {
+                val -= elements.removeFirst().value;
+            }
+            if (elements.isEmpty()) {
+                val = null;
+            }
+        }
+
+        public int[] resultAsSubsequence() {
+            return toIntArray(this.elements);
+        }
     }
 }
--- src/test/java/com/acabra/gtechdevalgs/litcode/arrays/ConsecutiveSumTest.java
+++ src/test/java/com/acabra/gtechdevalgs/litcode/arrays/ConsecutiveSumTest.java
@@ -47,4 +47,76 @@ class ConsecutiveSumTest {
         int[] expected = new int[]{};
         Assertions.assertArrayEquals(expected, underTest.consecutiveSum(arr, sum));
     }
+
+    @Test
+    public void must_return_33() {
+        int[] arr =  new int[]{1, 5, 3, 0, 7, 2, 4};
+        int sum = 0;
+        int[] expected = new int[]{0};
+        Assertions.assertArrayEquals(expected, underTest.consecutiveSum(arr, sum));
+    }
+
+    @Test
+    public void must_return_25_index() {
+        int[] arr =  new int[]{1, 5, 3, 0, 7, 2, 4};
+        int sum = 12;
+        int[] expected = new int[]{2,5};
+        Assertions.assertArrayEquals(expected, underTest.consecutiveSumIndex(arr, sum));
+    }
+
+    @Test
+    public void must_return_02_index() {
+        int[] arr =  new int[]{1, 5, 3, 0, 7, 2, 4};
+        int sum = 9;
+        int[] expected = new int[]{0, 2};
+        Assertions.assertArrayEquals(expected, underTest.consecutiveSumIndex(arr, sum));
+    }
+
+    @Test
+    public void must_return_00_index() {
+        int[] arr =  new int[]{1, 5, 3, 0, 7, 2, 4};
+        int sum = 1;
+        int[] expected = new int[]{0, 0};
+        Assertions.assertArrayEquals(expected, underTest.consecutiveSumIndex(arr, sum));
+    }
+
+    @Test
+    public void must_return_empty_index() {
+        int[] arr =  new int[]{1, 5, 3, 0, 7, 2, 4};
+        int sum = 23;
+        int[] expected = new int[]{};
+        Assertions.assertArrayEquals(expected, underTest.consecutiveSumIndex(arr, sum));
+    }
+
+    @Test
+    public void must_return_33_index() {
+        int[] arr =  new int[]{1, 5, 3, 0, 7, 2, 4};
+        int sum = 0;
+        int[] expected = new int[]{3, 3};
+        Assertions.assertArrayEquals(expected, underTest.consecutiveSumIndex(arr, sum));
+    }
+
+    @Test
+    public void must_return_12_index() {
+        int[] arr =  new int[]{3, 2, 4};
+        int sum = 6;
+        int[] expected = new int[]{1, 2};
+        Assertions.assertArrayEquals(expected, underTest.consecutiveSumIndex(arr, sum));
+    }
+
+    @Test
+    public void must_return_24_index() {
+        int[] arr =  new int[]{1, 1, 3, 0, 3, 2, 4};
+        int sum = 6;
+        int[] expected = new int[]{2, 4};
+        Assertions.assertArrayEquals(expected, underTest.consecutiveSumIndex(arr, sum));
+    }
+
+    @Test
+    public void must_return_22_index() {
+        int[] arr =  new int[]{5, 1, 0, 0, 0, 0, 4};
+        int sum = 0;
+        int[] expected = new int[]{2, 2};
+        Assertions.assertArrayEquals(expected, underTest.consecutiveSumIndex(arr, sum));
+    }
 }
